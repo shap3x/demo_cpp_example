7db85d280e2ba9086f89afc73af6b3b06ef8af72
shap3x <itsmefrancescoromano@gmail.com>
2025-10-14 12:11:08 +0200

    fixed error on not found resource .gdextension

--- demo/main.gd
+++ demo/main.gd
@@ -3,7 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	var summator = Summator.new()
+	summator.add(5)
+	summator.add(2)
+	print(summator.get_total())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
