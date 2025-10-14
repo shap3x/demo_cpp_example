9b93f8935fdd93594e1c62d333dedf1218186449
Francesco <itsmefrancescoromano@gmail.com>
2025-10-14 12:18:52 +0200

    Feature/setup gdextension (#1)
    * Added register types and gdextension configuration
    
    * fixed error on not found resource .gdextension
    
    * added gdextension

---
+++ demo/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var summator = Summator.new()
+	summator.add(5)
+	summator.add(2)
+	print(summator.get_total())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
