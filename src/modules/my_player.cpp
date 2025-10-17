44f3045d71442f894a00eef0850785e4c5d4b43a
shap3x <itsmefrancescoromano@gmail.com>
2025-10-17 16:24:36 +0200

    End gdextension template

---
+++ src/modules/my_player.cpp
@@ -0,0 +1,43 @@
+#include "modules/my_player.h"
+
+#include <godot_cpp/classes/input.hpp>
+
+void MyPlayer::_bind_methods(){
+    ClassDB::bind_method(D_METHOD("set_speed","type"), &MyPlayer::set_speed);
+    ClassDB::bind_method(D_METHOD("get_speed"), &MyPlayer::get_speed);
+
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "speed"),"set_speed","get_speed");
+}
+
+void MyPlayer::_physics_process(double delta) {
+    Input *input = Input::get_singleton();
+    Vector2 velocity;
+
+    if(input->is_action_pressed("ui_up")) {
+        velocity.y = -1.0;
+    }
+    if(input->is_action_pressed("ui_down")) {
+        velocity.y = 1.0;
+    }
+    if(input->is_action_pressed("ui_left")) {
+        velocity.x = -1.0;
+    }
+    if(input->is_action_pressed("ui_right")) {
+        velocity.x = 1.0;
+    }
+
+    set_velocity(speed * velocity);
+    move_and_slide();
+}
+
+void MyPlayer::set_speed(const float speed){
+    this->speed = speed;
+
+}
+float MyPlayer::get_speed() const{
+    return speed;
+}
+
+MyPlayer::MyPlayer(){
+
+}
